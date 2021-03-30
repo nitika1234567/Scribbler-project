dcc16361978c4c9122af6b1d5f838fb86920f84a
nitika1234567 <nitika.cse18@chitkarauniversity.edu.in>
2021-03-30 11:43:42 +0530

    scribbler project

---
+++ script/header.js
@@ -0,0 +1,22 @@
+// JavaScript source code
+function showSignInModal() {
+    var modal = document.getElementById("signin-modal");
+    modal.style.display = "flex";
+}
+function hideSignInModal() {
+    var modal = document.getElementById("signin-modal");
+    modal.style.display = "none";
+}
+function showSignUpModal() {
+    var modal = document.getElementById("signup-modal");
+    modal.style.display = "flex";
+}
+function hideSignUpModal() {
+    var modal = document.getElementById("signup-modal");
+    modal.style.display = "none";
+}
+
+function showCreatePost() {
+    var modal = document.getElementById("create-post-modal");
+    modal.style.display = "flex";
+}
